35a6478613a1177ce72f4c067f652cbb8a992c69
Yusuke Yachi <yyachi@misasa.okayama-u.ac.jp>
2018-11-15 08:27:01 +0900

    add dream link

--- app/models/target.rb
+++ app/models/target.rb
@@ -3,4 +3,7 @@ class Target < ActiveRecord::Base
   has_many :session_targets, dependent: :destroy
 
   validates :global_id, presence: true, allow_blank: false
+  def query_path
+    "https://dream.misasa.okayama-u.ac.jp/?q=#{self.global_id}"
+  end
 end
