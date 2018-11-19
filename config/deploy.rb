06beb28ae43d85fb88d53c3f9d013ec4be228a1d
Yusuke Yachi <yyachi@misasa.okayama-u.ac.jp>
2018-11-19 15:51:19 +0900

    apply help icon

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/srv/machine_time'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system config/unicorn}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system config/unicorn config/settings}
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
